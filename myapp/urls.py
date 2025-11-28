df59e8ee8667c40b5d3587f3aad4de90b22bf5e7
Dagnachew Wondu Belachew <dagnachew.wondu@coopbankoromiasc.com>
2025-11-28 15:38:52 +0300

    tables

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name = "home")
-    
+    path('', views.dashboard, name='dashboard'),
+    path('products/', views.product_list, name='product_list'),
+    path('add-product/', views.add_product, name='add_product'),
+    path('add-stock/', views.add_stock, name='add_stock'),
+    path('add-sale/', views.add_sale, name='add_sale'),
+    path('sales/', views.sales_list, name='sales_list'),
+    path('expired/', views.expired_list, name='expired_list'),
 ]
