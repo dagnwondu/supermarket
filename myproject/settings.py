6926c342ce970c1593e5956b762328a6d2497453
Dagnachew Wondu <waysingtoncom@godfather.hostns.io>
2025-12-24 11:15:49 +0000

    Force sync all local files

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,13 +78,23 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'waysingtoncom_supermarket',
+        'USER': 'waysingtoncom_dagn',
+        'PASSWORD': 'Ofret@2112',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
